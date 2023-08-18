33b5e857fe5da2af0ba1376d3a8ac043f8c510c8
shaowenchen <mail@chenshaowen.com>
2023-08-18 19:18:53 +0800

    fix index.html

--- web/router.go
+++ web/router.go
@@ -9,7 +9,7 @@ func SetupRouter(r *gin.Engine) {
 	r.StaticFile("/favicon.ico", "./web/dist/favicon.ico")
 	r.StaticFile("/logo.png", "./web/dist/logo.png")
 	r.Static("/assets", "./web/dist/assets")
-	r.LoadHTMLFiles("./web/dist/*.html")
+	r.LoadHTMLFiles("./web/dist/index.html")
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
